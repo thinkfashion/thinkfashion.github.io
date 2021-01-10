9bf31c608d93a30b23745232e175554da090f4d6
Scott Meisburger <smeisburger@gmail.com>
2021-01-09 23:39:10 -0500

    footer, more fetching

--- terrafirma/layout.js
+++ terrafirma/layout.js
@@ -1,52 +1,51 @@
 var container = document.getElementById("container");
-var boxes = container.children;
-var clones = [];
+var footer = document.getElementById("footer");
+var musicBox = document.getElementById("music-box");
+var boxes = container.getElementsByClassName('box');
+
 var boxHeight = parseInt(window.getComputedStyle(boxes[0]).getPropertyValue('height'));
 var boxWidth = parseInt(window.getComputedStyle(boxes[0]).getPropertyValue('width'));
 boxWidth = boxWidth + parseInt(window.getComputedStyle(boxes[0]).getPropertyValue('margin-left'));
 boxWidth = boxWidth + parseInt(window.getComputedStyle(boxes[0]).getPropertyValue('margin-right'));
-
-function addClones(num, className) {
-    var lastBox = boxes[boxes.length - 1];
-    for (i = 0; i < num; i++) {
-	var clone = lastBox.cloneNode(true);
-	clone.classList.add('placeholder');
-	clones.push(clone);
-	lastBox.after(clone);
-    }
-}
-
-function removeClones() {
-    for (var i = 0; i < clones.length; i++) {
-	clones[i].parentNode.removeChild(clones[i]);
-    }
-    clones = [];
-}
 
 var prevCols = 0;
 
 function reportWindowSize() {
     var containerWidth = parseInt(window.getComputedStyle(container).getPropertyValue('width'));
     var cols = Math.floor(containerWidth / boxWidth);
-    var remainder = (boxes.length - clones.length) % cols
+    var remainder = boxes.length % cols
     if (prevCols !== cols) {
-	removeClones();
+	footer.classList.remove('lift');
 	if (remainder !== 0) {
-	    addClones(cols - remainder);
+	    footer.classList.add('lift');
 	}
-	// console.log('cols', cols, 'clones', clones.length);
+	console.log('cols', cols, 'boxes', boxes.length, 'remainer', remainder);
     }
     prevCols = cols;
 }
 
-var protoBox = boxes[0].cloneNode(true);
+var protoBox = container.children[0].cloneNode(true);
+protoBox.removeAttribute('style');
 
 var makeTwitterURL = function(uname) {
     return 'https://twitter.com/' + uname.replace('@', '');
 }
 
-var renderBox = function(title, items) {
+var renderMusicBox = function(item) {
+    var embedStr = item[keys.indexOf('URL')];
+    if (embedStr != '') {
+	var embedElem = document.createElement('div');
+	embedElem.classList.add('embed');
+	embedElem.innerHTML = embedStr;
+	embedElem.children[0].setAttribute('width', 208);
+	embedElem.children[0].setAttribute('height', 117);
+	musicBox.querySelector('.box-content').appendChild(embedElem);
+    }
+}
+
+var renderBox = function(title, items, className) {
     var elem = protoBox.cloneNode(true);
+    elem.classList.add(className);
     elem.getElementsByTagName('h1')[0].textContent = title;
 
     for (var i = 0; i < items.length; i++) {
@@ -101,65 +100,87 @@ var renderBox = function(title, items) {
 	liElem.appendChild(svgElem);
         elem.getElementsByTagName('ul')[0].appendChild(liElem);
     }
-
+    
     container.appendChild(elem);
 }
 
-// window.onresize = reportWindowSize;
-// reportWindowSize();
+window.onresize = reportWindowSize;
 
 // https://developers.google.com/sheets/api/reference/rest
 
 var API_KEY = "AIzaSyAZKpXAdjYOcSUrunD1AJIVXvSwRXYYynk";
 var sheet_id = "1VsbC0dGsGWi4PIu3IsKhvdx2C3qbfWG7qzPc-AfMISw";
 
-var values = "2021_01!A1:L100";
-var url = "https://sheets.googleapis.com/v4/spreadsheets/" + sheet_id;
-url += "/values/" + values + "?key=" + API_KEY;
+var make_url = function(values) {
+    var url = "https://sheets.googleapis.com/v4/spreadsheets/" + sheet_id;
+    url += "/values/" + values + "?key=" + API_KEY;
+    return url;
+}
 
-var categories = {};
+var categories = [];
+var sheet = {};
 var keys = [];
 
-let promise = fetch(url);
-promise.then(response => {
-    if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-                    response.status);
-        return;
+Promise.all([
+    fetch(make_url('categories')),
+    fetch(make_url('2021_01')),
+]).then(function (responses) {
+    // Get a JSON object from each of the responses
+    return Promise.all(responses.map(function (response) {
+	return response.json();
+    }));
+}).then(function (data) {
+    // Log the data to the console
+    // You would do something with both sets of data here
+    console.log(data);
+    // first request
+    let values = data[0]['values'];
+    for (var i = 1; i < values.length; i++) {
+	categories.push(values[i]);
     }
-    response.json().then(data => {
-	var values = data['values'];
-	keys = values.shift();
-	for (var i = 0; i < values.length; i++) {
-	    for (var j = 0; j < values[i].length; j++) {
-		if (keys[j] === 'Category') {
-		    if (values[i][j] === '') {
-			console.log("skipping item missing category: ", values[i]);
-			continue;
-		    }
-		    var c = values[i][j];
-		    if (c in categories) {
-			categories[c].push(values[i]);
-		    } else {
-			categories[c] = [values[i]];
-		    }
+
+    values = data[1]['values'];
+    let musicItem = null;
+    keys = values.shift();
+    for (var i = 0; i < values.length; i++) {
+	for (var j = 0; j < values[i].length; j++) {
+	    if (keys[j] === 'Category') {
+		var c = values[i][j];
+		if (c === '') {
+		    console.log('skipping item missing category: ', values[i]);
+		    continue;
+		}
+		if (c == 'Music') {
+		    musicItem = values[i];
+		    continue;
+		}
+		if (c in sheet) {
+		    sheet[c].push(values[i]);
+		} else {
+		    sheet[c] = [values[i]];
 		}
 	    }
 	}
+    }
 
-	var categoryArray = Object.keys(categories);
-	for (var i = 0; i < categoryArray.length; i++) {
-	    if (categoryArray[i] == 'Music') {
-		continue;
-	    }
-	    var elem = null;
-	    if (i < boxes.length) {
-		boxes[i].parentNode.removeChild(boxes[i]);
-	    }
-	    renderBox(categoryArray[i], categories[categoryArray[i]]);
+    if (musicItem != null) {
+	renderMusicBox(musicItem);
+    }
+
+    for (var i = 0; i < categories.length; i++) {
+	var elem = null;
+	if (i < boxes.length) {
+	    boxes[i].parentNode.removeChild(boxes[i]);
 	}
-	// reportWindowSize();
-    }).catch(error => {
-        console.log(error.message);
-    })
-})
+	renderBox(categories[i][0], sheet[categories[i][0]], categories[i][1]);
+    }
+    for (var i = 0; i < 3; i++) {
+	var emptySpaceElem = document.createElement('div');
+	emptySpaceElem.classList.add('empty-space-childs');
+	container.appendChild(emptySpaceElem);
+    }
+    reportWindowSize();
+}).catch(function (error) {
+    // if there's an error, log it
+    console.log(error);
+});
